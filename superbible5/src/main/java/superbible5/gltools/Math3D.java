e87fb97a4af0d75740b63b1552bbe0f8dbeed755
Ayco Holleman <ayco_holleman@yahoo.com>
2015-03-22 19:09:50 +0100

    math3d

--- superbible5/src/main/java/superbible5/gltools/Math3D.java
+++ superbible5/src/main/java/superbible5/gltools/Math3D.java
@@ -1,156 +1,163 @@
 package superbible5.gltools;
 
-import org.lwjgl.util.vector.Matrix4f;
-import org.lwjgl.util.vector.ReadableVector3f;
-import org.lwjgl.util.vector.Vector3f;
-import org.lwjgl.util.vector.Vector4f;
-import org.lwjgl.util.vector.WritableVector3f;
-
 public final class Math3D {
 
-	public static final int SIZE_OF_FLOAT = Float.SIZE >> 3;
-	public static final int SIZE_OF_INT = Integer.SIZE >> 3;
-	public static final int SIZE_OF_VEC2 = 2 * SIZE_OF_FLOAT;
-	public static final int SIZE_OF_VEC3 = 3 * SIZE_OF_FLOAT;
+	public static final double M3D_PI = Math.PI;
+	public static final double M3D_2PI = 2D * M3D_PI;
+	public static final double M3D_PI_DIV_180 = M3D_PI / 180D;
+	public static final double M3D_INV_PI_DIV_180 = 180D / M3D_PI;
 
 
 	//@formatter:off
-	public static void set0(Matrix4f target, float f) { target.m00 = f; }
-	public static void set1(Matrix4f target, float f) { target.m01 = f; }
-	public static void set2(Matrix4f target, float f) { target.m02 = f; }
-	public static void set3(Matrix4f target, float f) { target.m03 = f; }
-	public static void set4(Matrix4f target, float f) { target.m10 = f; }
-	public static void set5(Matrix4f target, float f) { target.m11 = f; }
-	public static void set6(Matrix4f target, float f) { target.m12 = f; }
-	public static void set7(Matrix4f target, float f) { target.m13 = f; }
-	public static void set8(Matrix4f target, float f) { target.m20 = f; }
-	public static void set9(Matrix4f target, float f) { target.m21 = f; }
-	public static void set10(Matrix4f target, float f) { target.m22 = f; }
-	public static void set11(Matrix4f target, float f) { target.m23 = f; }
-	public static void set12(Matrix4f target, float f) { target.m30 = f; }
-	public static void set13(Matrix4f target, float f) { target.m31 = f; }
-	public static void set14(Matrix4f target, float f) { target.m32 = f; }
-	public static void set15(Matrix4f target, float f) { target.m33 = f; }
+	public static float  m3dDegToRad(float x)	{ return (float) (x * M3D_PI_DIV_180); }
+	public static double m3dDegToRad(double x)	{ return x * M3D_PI_DIV_180; }
 	
-	public static float get0(Matrix4f m) { return m.m00; }
-	public static float get1(Matrix4f m) { return m.m01; }
-	public static float get2(Matrix4f m) { return m.m02; }
-	public static float get3(Matrix4f m) { return m.m03; }
-	public static float get4(Matrix4f m) { return m.m10; }
-	public static float get5(Matrix4f m) { return m.m11; }
-	public static float get6(Matrix4f m) { return m.m12; }
-	public static float get7(Matrix4f m) { return m.m13; }
-	public static float get8(Matrix4f m) { return m.m20; }
-	public static float get9(Matrix4f m) { return m.m21; }
-	public static float get10(Matrix4f m) { return m.m22; }
-	public static float get11(Matrix4f m) { return m.m23; }
-	public static float get12(Matrix4f m) { return m.m30; }
-	public static float get13(Matrix4f m) { return m.m31; }
-	public static float get14(Matrix4f m) { return m.m32; }
-	public static float get15(Matrix4f m) { return m.m33; }
+	public static float  m3dRadToDeg(float x)	{ return (float) (x * M3D_INV_PI_DIV_180); }
+	public static double m3dRadToDeg(double x)	{ return x * M3D_INV_PI_DIV_180; }
 	
-	public static void col0(Matrix4f target, Vector3f vec) { set0(target,vec.x); set1(target,vec.y); set2(target,vec.z); }
-	public static void col1(Matrix4f target, Vector3f vec) { set4(target,vec.x); set5(target,vec.y); set6(target,vec.z); }
-	public static void col2(Matrix4f target, Vector3f vec) { set8(target,vec.x); set9(target,vec.y); set10(target,vec.z); }
-	public static void col3(Matrix4f target, Vector3f vec) { set12(target,vec.x); set13(target,vec.y); set14(target,vec.z); }
+	public static float  m3dHrToDeg(float x)	{ return (float) (x * (1F / 15F)); }
+	public static double m3dHrToDeg(double x)	{ return x * (1D / 15D); }
 	
-	public static void col0(Matrix4f target, Vector4f vec) { set0(target,vec.x); set1(target,vec.y); set2(target,vec.z); set3(target,vec.w); }
-	public static void col1(Matrix4f target, Vector4f vec) { set4(target,vec.x); set5(target,vec.y); set6(target,vec.z); set7(target,vec.w); }
-	public static void col2(Matrix4f target, Vector4f vec) { set8(target,vec.x); set9(target,vec.y); set10(target,vec.z); set11(target,vec.w); }
-	public static void col3(Matrix4f target, Vector4f vec) { set12(target,vec.x); set13(target,vec.y); set14(target,vec.z); set15(target,vec.w); }
+	public static float  m3dHrToRad(float x) 	{ return m3dDegToRad(m3dHrToDeg(x)); }
+	public static double m3dHrToRad(double x) 	{ return m3dDegToRad(m3dHrToDeg(x)); }
+	
+	public static float  m3dDegToHr(float x)	{ return x * 15F; }
+	public static double m3dDegToHr(double x)	{ return x * 15D; }
+	
+	public static float m3dRadToHr(float x)		{ return m3dDegToHr(m3dRadToDeg(x)); }
+	public static double m3dRadToHr(double x)	{ return m3dDegToHr(m3dRadToDeg(x)); }	
 	//@formatter:on
 
-	private Math3D()
+	public static int m3dIsPOW2(int iValue)
 	{
+		int nPow2 = 1;
+		while (iValue > nPow2) {
+			nPow2 = (nPow2 << 1);
+		}
+		return nPow2;
 	}
 
 
-	public static boolean m3dCloseEnough(float candidate, float compareTo, float epsilon)
+	//@formatter:off
+	public static void m3dLoadVector2(float[] v, float x, float y) 
+		{ v[0] = x; v[1] = y; }
+	public static void m3dLoadVector3(float[] v, float x, float y, float z)
+		{ v[0] = x; v[1] = y; v[2] = z; }
+	public static void m3dLoadVector4(float[] v, float x, float y, float z, float w)
+		{ v[0] = x; v[1] = y; v[2] = z; v[3] = w; }
+	
+	public static void m3dLoadVector2(double[] v, double x, double y) 
+		{ v[0] = x; v[1] = y; }
+	public static void m3dLoadVector3(double[] v, double x, double y, double z)
+		{ v[0] = x; v[1] = y; v[2] = z; }
+	public static void m3dLoadVector4(double[] v, double x, double y, double z, double w)
+		{ v[0] = x; v[1] = y; v[2] = z; v[3] = w; }
+
+	
+	
+	public static void	m3dCopyVector2(float[] dst, float[] src) { memcpy2(dst, src); }
+	public static void	m3dCopyVector2(double[] dst, double[] src) { memcpy2(dst, src); }
+
+	public static void	m3dCopyVector3(float[] dst, float[] src) { memcpy3(dst, src); }
+	public static void	m3dCopyVector3(double[] dst, double[] src) { memcpy3(dst, src); }
+
+	public static void	m3dCopyVector4(float[] dst, float[] src) { memcpy4(dst, src); }
+	public static void	m3dCopyVector4(double[] dst, double[] src) { memcpy4(dst, src); }
+
+	
+	
+	public static void m3dAddVectors2(float[] r, float[] a, float[] b)
+		{ r[0] = a[0] + b[0]; r[1] = a[1] + b[1]; }
+	public static void m3dAddVectors2(double[] r, double[] a, double[] b)
+		{ r[0] = a[0] + b[0]; r[1] = a[1] + b[1]; }
+	
+	public static void m3dAddVectors3(float[] r, float[] a, float[] b)
+		{ r[0] = a[0] + b[0]; r[1] = a[1] + b[1]; r[2] = a[2] + b[2]; }
+	public static void m3dAddVectors3(double[] r, double[] a, double[] b)
+		{ r[0] = a[0] + b[0]; r[1] = a[1] + b[1]; r[2] = a[2] + b[2]; }
+	
+	public static void m3dAddVectors4(float[] r, float[] a, float[] b)
+		{ r[0] = a[0] + b[0]; r[1] = a[1] + b[1]; r[2] = a[2] + b[2]; r[3] = a[3] + b[3]; }
+	public static void m3dAddVectors4(double[] r, double[] a, double[] b)
+		{ r[0] = a[0] + b[0]; r[1] = a[1] + b[1]; r[2] = a[2] + b[2]; r[3] = a[3] + b[3]; }
+
+	
+
+	public static void m3dSubtractVectors2(float[] r, float[] a, float[] b)
+		{ r[0] = a[0] - b[0]; r[1] = a[1] - b[1];  }
+	public static void m3dSubtractVectors2(double[] r, double[] a, double[] b)
+		{ r[0] = a[0] - b[0]; r[1] = a[1] - b[1]; }
+	
+	public static void m3dSubtractVectors3(float[] r, float[] a, float[] b)
+		{ r[0] = a[0] - b[0]; r[1] = a[1] - b[1]; r[2] = a[2] - b[2]; }
+	public static void m3dSubtractVectors3(double[] r, double[] a, double[] b)
+		{ r[0] = a[0] - b[0]; r[1] = a[1] - b[1]; r[2] = a[2] - b[2]; }
+	
+	public static void m3dSubtractVectors4(float[] r, float[] a, float[] b)
+		{ r[0] = a[0] - b[0]; r[1] = a[1] - b[1]; r[2] = a[2] - b[2]; r[3] = a[3] - b[3]; }
+	public static void m3dSubtractVectors4(double[] r, double[] a, double[] b)
+		{ r[0] = a[0] - b[0]; r[1] = a[1] - b[1]; r[2] = a[2] - b[2]; r[3] = a[3] - b[3]; }
+
+	
+	
+	public static void m3dScaleVector2(float[] v, float scale) 
+		{ v[0] *= scale; v[1] *= scale; }
+	public static void m3dScaleVector2(double[] v, double scale) 
+		{ v[0] *= scale; v[1] *= scale; }
+	
+	public static void m3dScaleVector3(float[] v, float scale) 
+		{ v[0] *= scale; v[1] *= scale; v[2] *= scale; }
+	public static void m3dScaleVector3(double[] v, double scale) 
+		{ v[0] *= scale; v[1] *= scale; v[2] *= scale; }
+	
+	public static void m3dScaleVector4(float[] v, float scale) 
+		{ v[0] *= scale; v[1] *= scale; v[2] *= scale; v[3] *= scale; }
+	public static void m3dScaleVector4(double[] v, double scale) 
+		{ v[0] *= scale; v[1] *= scale; v[2] *= scale; v[3] *= scale; }	
+	//@formatter:on
+
+	public static void m3dCrossProduct3(float[] result, float[] u, float[] v)
 	{
-		return abs(candidate - compareTo) > epsilon;
+		result[0] = u[1] * v[2] - v[1] * u[2];
+		result[1] = -u[0] * v[2] + v[0] * u[2];
+		result[2] = u[0] * v[1] - v[0] * u[1];
 	}
 
 
-	public static void m3dCopyVector3(Vector3f dst, Vector3f src)
+	public static void m3dCrossProduct3(double[] result, double[] u, double[] v)
 	{
-		dst.x = src.x;
-		dst.y = src.y;
-		dst.z = src.z;
+		result[0] = u[1] * v[2] - v[1] * u[2];
+		result[1] = -u[0] * v[2] + v[0] * u[2];
+		result[2] = u[0] * v[1] - v[0] * u[1];
 	}
 
 
-	private static float abs(float f)
+	public static float m3dDotProduct3(float[] u, float[] v)
 	{
-		return Math.abs(f);
+		return u[0] * v[0] + u[1] * v[1] + u[2] * v[2];
 	}
 
 
-	public static void m3dMakeOrthographicMatrix(Matrix4f mProjection, float xMin, float xMax, float yMin,
-			float yMax, float zMin, float zMax)
+	public static double m3dDotProduct3(double[] u, double[] v)
 	{
-		m3dLoadIdentity44(mProjection);
-
-		set0(mProjection, 2.0f / (xMax - xMin));
-		set5(mProjection, 2.0f / (yMax - yMin));
-		set10(mProjection, -2.0f / (zMax - zMin));
-		set12(mProjection, -((xMax + xMin) / (xMax - xMin)));
-		set13(mProjection, -((yMax + yMin) / (yMax - yMin)));
-		set14(mProjection, -((zMax + zMin) / (zMax - zMin)));
-		set15(mProjection, 1.0f);
+		return u[0] * v[0] + u[1] * v[1] + u[2] * v[2];
 	}
 
 
-	private static void m3dLoadIdentity44(Matrix4f m)
-	{
-		m.setIdentity();
-	}
-
-
-	public static void m3dTransformVector4(Vector4f vOut, Vector4f v, Matrix4f m)
-	{
-		Matrix4f.transform(m, v, vOut);
-	}
-
-
-	/////////////////////////////////////////////////////////////////////////////////////////
-	//Calculate the plane equation of the plane that the three specified points lay in. The
-	//points are given in clockwise winding order, with normal pointing out of clockwise face
-	//planeEq contains the A,B,C, and D of the plane equation coefficients
-	public static void m3dGetPlaneEquation(Vector4f planeEq, Vector4f p1, Vector4f p2, Vector4f p3)
-	{
-		Vector3f v1 = new Vector3f();
-		Vector3f v2 = new Vector3f();
-
-		// V1 = p3 - p1
-		v1.x = p3.x - p1.x;
-		v1.y = p3.y - p1.y;
-		v1.z = p3.z - p1.z;
-
-		// V2 = P2 - p1
-		v2.x = p2.x - p1.x;
-		v2.y = p2.y - p1.y;
-		v2.z = p2.z - p1.z;
-
-		// Unit normal to plane - Not sure which is the best way here
-		m3dCrossProduct3(planeEq, v1, v2);
-		m3dNormalizeVector3(planeEq);
-
-		// Back substitute to get D
-		planeEq.w = -(planeEq.x * p3.x + planeEq.y * p3.y + planeEq.z * p3.z);
-	}
-
-
-	private static void m3dNormalizeVector3(Vector4f u)
-	{
-		u.normalise();
-	}
-
-
-	public static void m3dCrossProduct3(WritableVector3f result, ReadableVector3f u, ReadableVector3f v)
-	{
-		result.setX(u.getY() * v.getZ() - v.getY() * u.getZ());
-		result.setY(-u.getX() * v.getZ() + v.getX() * u.getZ());
-		result.setZ(u.getX() * v.getY() - v.getX() * u.getY());
-	}
+	//@formatter:off
+	public static void memcpy2(float[] dst, float[] src)
+		{ dst[0] = src[0]; dst[1] = src[1]; }
+	public static void memcpy3(float[] dst, float[] src)
+		{ dst[0] = src[0]; dst[1] = src[1]; dst[2] = src[2]; }
+	public static void memcpy4(float[] dst, float[] src)
+		{ dst[0] = src[0]; dst[1] = src[1]; dst[2] = src[2]; dst[3] = src[3]; }
+	
+	public static void memcpy2(double[] dst, double[] src)
+		{ dst[0] = src[0]; dst[1] = src[1]; }
+	public static void memcpy3(double[] dst, double[] src)
+		{ dst[0] = src[0]; dst[1] = src[1]; dst[2] = src[2]; }
+	public static void memcpy4(double[] dst, double[] src)
+		{ dst[0] = src[0]; dst[1] = src[1]; dst[2] = src[2]; dst[3] = src[3]; }
+	//@formatter:on
 
 }
