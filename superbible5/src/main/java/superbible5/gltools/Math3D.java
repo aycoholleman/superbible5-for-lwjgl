919a929eb31b95bdbe0110be2b9c0886b04ad6e7
Ayco Holleman <ayco_holleman@yahoo.com>
2015-03-23 21:10:08 +0100

    added math3d functions

--- superbible5/src/main/java/superbible5/gltools/Math3D.java
+++ superbible5/src/main/java/superbible5/gltools/Math3D.java
@@ -1,6 +1,5 @@
 package superbible5.gltools;
 
-
 public final class Math3D {
 
 	public static final double M3D_PI = Math.PI;
@@ -452,10 +451,339 @@ public final class Math3D {
 	}
 
 
+	////////////////////////////////////////////////////////////////////////////
+	// Matrix functions
+	////////////////////////////////////////////////////////////////////////////
+
+	public static void m3dCopyMatrix33(float[] dst, float[] src)
+	{
+		memcpy9(dst, src);
+	}
+
+
+	public static void m3dCopyMatrix33(double[] dst, double[] src)
+	{
+		memcpy9(dst, src);
+	}
+
+
+	public static void m3dCopyMatrix44(float[] dst, float[] src)
+	{
+		memcpy16(dst, src);
+	}
+
+
+	public static void m3dCopyMatrix44(double[] dst, double[] src)
+	{
+		memcpy16(dst, src);
+	}
+
+
+	public static void m3dGetMatrixColumn33(float[] dst, float[] src, int column)
+	{
+	}
+
+
+	//	public static void m3dGetMatrixColumn33(double[] dst, M3DMatrix33d src, int column)
+	//	{ memcpy(dst, src + (3 * column), sizeof(double) * 3); }
+	//
+	//
+	//	public static void m3dSetMatrixColumn33(float[] dst, float[] src, int column)
+	//	{ memcpy(dst + (3 * column), src, sizeof(float) * 3); }
+	//
+	//
+	//	public static void m3dSetMatrixColumn33(M3DMatrix33d dst, double[] src, int column)
+	//	{ memcpy(dst + (3 * column), src, sizeof(double) * 3); }
+	//
+	//
+	//	public static void m3dGetMatrixColumn44(float[] dst, double[] src, int column)
+	//	{ memcpy(dst, src + (4 * column), sizeof(float) * 4); }
+	//
+	//
+	//	public static void m3dGetMatrixColumn44(double[] dst, M3DMatrix44d src, int column)
+	//	{ memcpy(dst, src + (4 * column), sizeof(double) * 4); }
+	//
+	//
+	//	public static void m3dSetMatrixColumn44(double[] dst, float[] src, int column)
+	//	{ memcpy(dst + (4 * column), src, sizeof(float) * 4); }
+	//
+	//
+	//	public static void m3dSetMatrixColumn44(M3DMatrix44d dst, double[] src, int column)
+	//	{ memcpy(dst + (4 * column), src, sizeof(double) * 4); }
+
+	////////////////////////////////////////////////////////////////////////////
+	// Helper functions (not within math3d.h or math3d.cpp)
+	////////////////////////////////////////////////////////////////////////////
+
 	public static void memcpy2(float[] dst, float[] src)
 	{
 		dst[0] = src[0];
 		dst[1] = src[1];
+	}
+
+
+	public static void memcpy2(double[] dst, double[] src)
+	{
+		dst[0] = src[0];
+		dst[1] = src[1];
+	}
+
+
+	/**
+	 * Equivalent to System.arraycopy with length parameter set to 3 (i.e. three
+	 * elements will be copied from src to dest).
+	 * 
+	 * @param dst
+	 *            Destination (write-to) array
+	 * @param dstPos
+	 *            Index from which to insert into destination array
+	 * @param src
+	 *            Source (read-from) array
+	 * @param srcPos
+	 *            Index from which the read the source array
+	 */
+	public static void memcpy3(float[] dst, int dstPos, float[] src, int srcPos)
+	{
+		// Actually we don't use System.arraycopy because for small arrays
+		// a manual copy is significantly faster
+		dst[dstPos + 0] = src[srcPos + 0];
+		dst[dstPos + 1] = src[srcPos + 1];
+		dst[dstPos + 2] = src[srcPos + 2];
+	}
+
+
+	public static void memcpy3(float[] dst, float[] src)
+	{
+		memcpy3(dst, 0, src, 0);
+	}
+
+
+	/**
+	 * Equivalent to System.arraycopy with length parameter set to 3 (i.e. three
+	 * elements will be copied from src to dest).
+	 * 
+	 * @param dst
+	 *            Destination (write-to) array
+	 * @param dstPos
+	 *            Index from which to insert into destination array
+	 * @param src
+	 *            Source (read-from) array
+	 * @param srcPos
+	 *            Index from which the read the source array
+	 */
+	public static void memcpy3(double[] dst, int dstPos, double[] src, int srcPos)
+	{
+		dst[dstPos + 0] = src[srcPos + 0];
+		dst[dstPos + 1] = src[srcPos + 1];
+		dst[dstPos + 2] = src[srcPos + 2];
+	}
+
+
+	public static void memcpy3(double[] dst, double[] src)
+	{
+		memcpy3(dst, 0, src, 0);
+	}
+
+
+	/**
+	 * Equivalent to System.arraycopy with length parameter set to 4 (i.e. four
+	 * elements will be copied from src to dest).
+	 * 
+	 * @param dst
+	 *            Destination (write-to) array
+	 * @param dstPos
+	 *            Index from which to insert into destination array
+	 * @param src
+	 *            Source (read-from) array
+	 * @param srcPos
+	 *            Index from which the read the source array
+	 */
+	public static void memcpy4(float[] dst, int dstPos, float[] src, int srcPos)
+	{
+		dst[dstPos + 0] = src[srcPos + 0];
+		dst[dstPos + 1] = src[srcPos + 1];
+		dst[dstPos + 2] = src[srcPos + 2];
+		dst[dstPos + 3] = src[srcPos + 3];
+	}
+
+
+	public static void memcpy4(float[] dst, float[] src)
+	{
+		memcpy4(dst, 0, src, 0);
+	}
+
+
+	/**
+	 * Equivalent to System.arraycopy with length parameter set to 4 (i.e. four
+	 * elements will be copied from src to dest).
+	 * 
+	 * @param dst
+	 *            Destination (write-to) array
+	 * @param dstPos
+	 *            Index from which to insert into destination array
+	 * @param src
+	 *            Source (read-from) array
+	 * @param srcPos
+	 *            Index from which the read the source array
+	 */
+	public static void memcpy4(double[] dst, int dstPos, double[] src, int srcPos)
+	{
+		dst[dstPos + 0] = src[srcPos + 0];
+		dst[dstPos + 1] = src[srcPos + 1];
+		dst[dstPos + 2] = src[srcPos + 2];
+		dst[dstPos + 3] = src[srcPos + 3];
+	}
+
+
+	public static void memcpy4(double[] dst, double[] src)
+	{
+		memcpy4(dst, 0, src, 0);
+	}
+
+
+	/**
+	 * Equivalent to System.arraycopy with length parameter set to 9 (i.e. nine
+	 * elements will be copied from src to dest).
+	 * 
+	 * @param dst
+	 *            Destination (write-to) array
+	 * @param dstPos
+	 *            Index from which to insert into destination array
+	 * @param src
+	 *            Source (read-from) array
+	 * @param srcPos
+	 *            Index from which the read the source array
+	 */
+	public static void memcpy9(float[] dst, int dstPos, float[] src, int srcPos)
+	{
+		dst[dstPos + 0] = src[srcPos + 0];
+		dst[dstPos + 1] = src[srcPos + 1];
+		dst[dstPos + 2] = src[srcPos + 2];
+		dst[dstPos + 3] = src[srcPos + 3];
+		dst[dstPos + 4] = src[srcPos + 4];
+		dst[dstPos + 5] = src[srcPos + 5];
+		dst[dstPos + 6] = src[srcPos + 6];
+		dst[dstPos + 7] = src[srcPos + 7];
+		dst[dstPos + 8] = src[srcPos + 8];
+	}
+
+
+	public static void memcpy9(float[] dst, float[] src)
+	{
+		memcpy9(dst, 0, src, 0);
+	}
+
+
+	/**
+	 * Equivalent to System.arraycopy with length parameter set to 9 (i.e. nine
+	 * elements will be copied from src to dest).
+	 * 
+	 * @param dst
+	 *            Destination (write-to) array
+	 * @param dstPos
+	 *            Index from which to insert into destination array
+	 * @param src
+	 *            Source (read-from) array
+	 * @param srcPos
+	 *            Index from which the read the source array
+	 */
+	public static void memcpy9(double[] dst, int dstPos, double[] src, int srcPos)
+	{
+		dst[dstPos + 0] = src[srcPos + 0];
+		dst[dstPos + 1] = src[srcPos + 1];
+		dst[dstPos + 2] = src[srcPos + 2];
+		dst[dstPos + 3] = src[srcPos + 3];
+		dst[dstPos + 4] = src[srcPos + 4];
+		dst[dstPos + 5] = src[srcPos + 5];
+		dst[dstPos + 6] = src[srcPos + 6];
+		dst[dstPos + 7] = src[srcPos + 7];
+		dst[dstPos + 8] = src[srcPos + 8];
+	}
+
+
+	public static void memcpy9(double[] dst, double[] src)
+	{
+		memcpy9(dst, 0, src, 0);
+	}
+
+
+	/**
+	 * Equivalent to System.arraycopy with length parameter set to 16 (i.e. 16
+	 * elements will be copied from src to dest).
+	 * 
+	 * @param dst
+	 *            Destination (write-to) array
+	 * @param dstPos
+	 *            Index from which to insert into destination array
+	 * @param src
+	 *            Source (read-from) array
+	 * @param srcPos
+	 *            Index from which the read the source array
+	 */
+	public static void memcpy16(float[] dst, int dstPos, float[] src, int srcPos)
+	{
+		dst[dstPos + 0] = src[srcPos + 0];
+		dst[dstPos + 1] = src[srcPos + 1];
+		dst[dstPos + 2] = src[srcPos + 2];
+		dst[dstPos + 3] = src[srcPos + 3];
+		dst[dstPos + 4] = src[srcPos + 4];
+		dst[dstPos + 5] = src[srcPos + 5];
+		dst[dstPos + 6] = src[srcPos + 6];
+		dst[dstPos + 7] = src[srcPos + 7];
+		dst[dstPos + 8] = src[srcPos + 8];
+		dst[dstPos + 9] = src[srcPos + 9];
+		dst[dstPos + 10] = src[srcPos + 10];
+		dst[dstPos + 11] = src[srcPos + 11];
+		dst[dstPos + 12] = src[srcPos + 12];
+		dst[dstPos + 13] = src[srcPos + 13];
+		dst[dstPos + 14] = src[srcPos + 14];
+		dst[dstPos + 15] = src[srcPos + 15];
+	}
+
+
+	public static void memcpy16(float[] dst, float[] src)
+	{
+		memcpy9(dst, 0, src, 0);
+	}
+
+
+	/**
+	 * Equivalent to System.arraycopy with length parameter set to 16 (i.e. 16
+	 * elements will be copied from src to dest).
+	 * 
+	 * @param dst
+	 *            Destination (write-to) array
+	 * @param dstPos
+	 *            Index from which to insert into destination array
+	 * @param src
+	 *            Source (read-from) array
+	 * @param srcPos
+	 *            Index from which the read the source array
+	 */
+	public static void memcpy16(double[] dst, int dstPos, double[] src, int srcPos)
+	{
+		dst[dstPos + 0] = src[srcPos + 0];
+		dst[dstPos + 1] = src[srcPos + 1];
+		dst[dstPos + 2] = src[srcPos + 2];
+		dst[dstPos + 3] = src[srcPos + 3];
+		dst[dstPos + 4] = src[srcPos + 4];
+		dst[dstPos + 5] = src[srcPos + 5];
+		dst[dstPos + 6] = src[srcPos + 6];
+		dst[dstPos + 7] = src[srcPos + 7];
+		dst[dstPos + 8] = src[srcPos + 8];
+		dst[dstPos + 9] = src[srcPos + 9];
+		dst[dstPos + 10] = src[srcPos + 10];
+		dst[dstPos + 11] = src[srcPos + 11];
+		dst[dstPos + 12] = src[srcPos + 12];
+		dst[dstPos + 13] = src[srcPos + 13];
+		dst[dstPos + 14] = src[srcPos + 14];
+		dst[dstPos + 15] = src[srcPos + 15];
+	}
+
+
+	public static void memcpy16(double[] dst, double[] src)
+	{
+		memcpy9(dst, 0, src, 0);
 	}
 
 
@@ -468,47 +796,6 @@ public final class Math3D {
 	public static double sqrt(double d)
 	{
 		return Math.sqrt(d);
-	}
-
-
-	public static void memcpy2(double[] dst, double[] src)
-	{
-		dst[0] = src[0];
-		dst[1] = src[1];
-	}
-
-
-	public static void memcpy3(float[] dst, float[] src)
-	{
-		dst[0] = src[0];
-		dst[1] = src[1];
-		dst[2] = src[2];
-	}
-
-
-	public static void memcpy3(double[] dst, double[] src)
-	{
-		dst[0] = src[0];
-		dst[1] = src[1];
-		dst[2] = src[2];
-	}
-
-
-	public static void memcpy4(float[] dst, float[] src)
-	{
-		dst[0] = src[0];
-		dst[1] = src[1];
-		dst[2] = src[2];
-		dst[3] = src[3];
-	}
-
-
-	public static void memcpy4(double[] dst, double[] src)
-	{
-		dst[0] = src[0];
-		dst[1] = src[1];
-		dst[2] = src[2];
-		dst[3] = src[3];
 	}
 
 }
